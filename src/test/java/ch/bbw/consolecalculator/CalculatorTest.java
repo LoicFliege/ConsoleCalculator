a8cf44c1e4661fcda21970296635cf9a3b931b69
Loïc Fliege <fliege.loic@gmail.com>
2020-01-20 11:25:29 +0100

    added @before setup

--- src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
@@ -2,31 +2,33 @@ package ch.bbw.consolecalculator;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
 	Calculator testee;
+	
+	@Before
+	public void setupCalculator() {
+		testee = new Calculator();
+	}
 
 	@Test
 	public void testSummeZweiPositivIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.adition(10, 25) == 35);
 	}
 	@Test
 	public void testSummeZweiNegativIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.adition(-10, -20) == -30);;
 	}
 	
 	@Test
 	public void testSubtractionZweiPositiveIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.subtraction(25,10) == 15);
 	}
 
 	@Test
 	public void testSubtractionZweiNegativIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.subtraction(25, -10) == 35);
 	}
 
