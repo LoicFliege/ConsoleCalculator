1b7c7bf8aed5747a8417dbe75b2160d65a87bc33
Loïc Fliege <fliege.loic@gmail.com>
2020-01-20 11:29:54 +0100

    Implemented Division Method in Calculator class

--- src/main/java/ch/bbw/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -13,4 +13,7 @@ public class Calculator {
 	public int subtraction(int value1, int value2) {
 		return value1 - value2;
 	}
+	public int division(int value1, int value2) {
+		return value1 / value2;
+	}
 }
